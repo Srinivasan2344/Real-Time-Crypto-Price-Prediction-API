828da0759e23f3a6e674b0ee3c0250bebeb9530f
LIFE OF RAM <115133778+Srinivasan2344@users.noreply.github.com>
2026-04-28 18:16:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,56 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 class CryptoInput(BaseModel):
     coin_name:str
-    open_price:float
-    close_price:float
-    high_price:float
-    low_price:float
-    volume:float
+    open_price:float = Field(...,gt=0)
+    close_price:float = Field(...,gt=0)
+    high_price:float = Field(...,gt=0)
+    low_price:float = Field(...,gt=0)
+    volume:float = Field(...,gt=0)
+
 
 @app.get("/")
 def home():
     return {"message":"API Working"}
 
+
 @app.post("/predict")
 def predict(data: CryptoInput):
-    return {
-        "prediction":"Bullish",
-        "confidence_score":0.72,
-        "volatility_level":"Medium"
-    }
+
+    try:
+
+        if data.low_price > data.high_price:
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid price input"
+            )
+
+        if data.close_price > data.open_price:
+            trend="Bullish"
+        else:
+            trend="Bearish"
+
+        return {
+            "prediction":trend,
+            "confidence_score":0.72,
+            "volatility_level":"Medium"
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
+
 
 @app.get("/market-insight")
-def market():
+def market_insight():
     return {
-      "Coin":"Bitcoin",
-      "Trend":"Bullish"
+        "Coin":"Bitcoin",
+        "Trend":"Bullish",
+        "Volatility":"Medium",
+        "Volume Trend":"Increasing"
     }
-    
